a109145b870bae7d77def3e72e5666a9dadb2ada
Rz Rasel <developer@Shurjo.local>
2018-06-03 14:11:39 +0600

    And Aim Game V-201805.00.1 - 2018-06-03 14:11:39

--- appusagesexample/src/main/java/com/rz/usagesexample/ActDashboard.java
+++ appusagesexample/src/main/java/com/rz/usagesexample/ActDashboard.java
@@ -12,12 +12,17 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Build;
 import android.provider.SyncStateContract;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.ImageView;
 import android.support.v7.widget.Toolbar;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
 
 import java.util.List;
 
@@ -27,7 +32,9 @@ public class ActDashboard extends AppCompatActivity {
     private Activity activity;
     private Context context;
     private Toolbar sysToolBar;
-    private int count = 0;
+    private DrawerLayout sysDrawerLayout;
+    private RelativeLayout sysIdDrawerContainer;
+    private ListView sysDrawerList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,81 +54,22 @@ public class ActDashboard extends AppCompatActivity {
         getSupportActionBar().setElevation(0);
         //getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#000000")));
         //|------------------------------------------------------------|
+        //|------------------------------------------------------------|
+        sysToolBar = (Toolbar) findViewById(R.id.sysToolBar);
+        /*sysDrawerLayout = (DrawerLayout) findViewById(R.id.sysDrawerLayout);
+        sysIdDrawerContainer = (RelativeLayout) findViewById(R.id.sysIdDrawerContainer);
+        //|------------------------------------------------------------|
+        LinearLayout idLinLayMainContainerView = (LinearLayout) findViewById(R.id.idLinLayMainContainerView);
+        //new DrawerListViewSetUp().onSetModelItems().onSetAdapter();*/
+        //|------------------------------------------------------------|
+        new OnDrawerSetup(activity, context)
+                .onSetToolbar(sysToolBar)
+                .onSetDrawerMenuFields()
+                .onSetDrawerMenuItems()
+                .onSetDrawerAdapter(true, Gravity.LEFT);
+        //|------------------------------------------------------------|
+        //|------------------------------------------------------------|
+        //|------------------------------------------------------------|
 
-    }
-
-    public static void setBadge(Context context, int count) {
-        String launcherClassName = getLauncherClassName(context);
-        if (launcherClassName == null) {
-            return;
-        }
-        Intent intent = new Intent("android.intent.action.BADGE_COUNT_UPDATE");
-        intent.putExtra("badge_count", count);
-        intent.putExtra("badge_count_package_name", context.getPackageName());
-        intent.putExtra("badge_count_class_name", launcherClassName);
-        context.sendBroadcast(intent);
-    }
-
-    public static String getLauncherClassName(Context context) {
-
-        PackageManager pm = context.getPackageManager();
-
-        Intent intent = new Intent(Intent.ACTION_MAIN);
-        intent.addCategory(Intent.CATEGORY_LAUNCHER);
-
-        List<ResolveInfo> resolveInfos = pm.queryIntentActivities(intent, 0);
-        for (ResolveInfo resolveInfo : resolveInfos) {
-            String pkgName = resolveInfo.activityInfo.applicationInfo.packageName;
-            if (pkgName.equalsIgnoreCase(context.getPackageName())) {
-                String className = resolveInfo.activityInfo.name;
-                return className;
-            }
-        }
-        return null;
-    }
-
-    /////////
-
-    ///////////
-
-
-    private void shortcutAdd(String name, int number) {
-        // Intent to be send, when shortcut is pressed by user ("launched")
-        Intent shortcutIntent = new Intent(getApplicationContext(), ActSplash.class);
-        shortcutIntent.setAction(SyncStateContract.Constants.ACCOUNT_NAME);
-
-        // Create bitmap with number in it -> very default. You probably want to give it a more stylish look
-        Bitmap bitmap = Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888);
-        Paint paint = new Paint();
-        paint.setColor(0xFF808080); // gray
-        paint.setTextAlign(Paint.Align.CENTER);
-        paint.setTextSize(50);
-        new Canvas(bitmap).drawText("" + number, 50, 50, paint);
-        ((ImageView) findViewById(R.id.icon)).setImageBitmap(bitmap);
-
-        // Decorate the shortcut
-        Intent addIntent = new Intent();
-        addIntent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcutIntent);
-        addIntent.putExtra(Intent.EXTRA_SHORTCUT_NAME, name);
-        addIntent.putExtra(Intent.EXTRA_SHORTCUT_ICON, bitmap);
-
-        // Inform launcher to create shortcut
-        addIntent.setAction("com.android.launcher.action.INSTALL_SHORTCUT");
-        getApplicationContext().sendBroadcast(addIntent);
-    }
-
-    private void shortcutDel(String name) {
-        // Intent to be send, when shortcut is pressed by user ("launched")
-        Intent shortcutIntent = new Intent(getApplicationContext(), ActSplash.class);
-        shortcutIntent.setAction(SyncStateContract.Constants.ACCOUNT_NAME);
-
-        // Decorate the shortcut
-        Intent delIntent = new Intent();
-        delIntent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcutIntent);
-        delIntent.putExtra(Intent.EXTRA_SHORTCUT_NAME, name);
-
-        // Inform launcher to remove shortcut
-        delIntent.setAction("com.android.launcher.action.UNINSTALL_SHORTCUT");
-        getApplicationContext().sendBroadcast(delIntent);
     }
 }
