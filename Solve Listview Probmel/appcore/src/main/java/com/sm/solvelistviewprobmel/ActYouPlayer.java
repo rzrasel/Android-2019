dc6cd915ea40dbba5cd4ee391d3f70ec5b419603
Rz Rasel <developer@Shurjo.local>
2018-07-23 18:35:44 +0600

    And Aim Game V-201805.00.1 - 2018-07-23 18:35:44

--- Solve Listview Probmel/appcore/src/main/java/com/sm/solvelistviewprobmel/ActYouPlayer.java
+++ Solve Listview Probmel/appcore/src/main/java/com/sm/solvelistviewprobmel/ActYouPlayer.java
@@ -14,12 +14,14 @@ import android.view.WindowManager;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerSupportFragment;
+import com.google.android.youtube.player.YouTubePlayerView;
 
 public class ActYouPlayer extends AppCompatActivity {
     private Activity activity;
     private Context context;
     public static final String YOUTUBE_API_KEY = "AIzaSyAqqIRxuAOuN1fsHCoc41-Lo0-XKiB8asc";
     private YouTubePlayerSupportFragment youTubePlayerSupportFragment;
+    private YouTubePlayerView youTubePlayerView;
     private YouTubePlayer youTubePlayer;
     private String videoId;
     private final static String TAG_FRAGMENT = "TAG_FRAGMENT";
@@ -75,6 +77,19 @@ public class ActYouPlayer extends AppCompatActivity {
             public void onInitializationFailure(YouTubePlayer.Provider argProvider, YouTubeInitializationResult argYouTubeInitializationResult) {
             }
         });
+        /*youTubePlayerView = (YouTubePlayerView) findViewById(R.id.sysYoutubePlayerView);
+        youTubePlayerView.initialize(YOUTUBE_API_KEY, new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider argProvider, YouTubePlayer argYouTubePlayer, boolean argWasRestored) {
+                if (!argWasRestored) {
+                    argYouTubePlayer.cueVideo(videoId);
+                }
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider argProvider, YouTubeInitializationResult argYouTubeInitializationResult) {
+            }
+        });*/
         //
         View decorView = getWindow().getDecorView();
         int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
@@ -83,9 +98,9 @@ public class ActYouPlayer extends AppCompatActivity {
                 | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
-        decorView.setSystemUiVisibility(uiOptions);
+        //decorView.setSystemUiVisibility(uiOptions);
         //
-        activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         activity.setRequestedOrientation(LANDSCAPE_ORIENTATION);
     }
 
@@ -102,3 +117,5 @@ public class ActYouPlayer extends AppCompatActivity {
         return;
     }
 }
+//https://stackoverflow.com/questions/4936553/android-how-can-you-align-a-button-at-the-bottom-and-listview-above/4936732
+//http://stacktips.com/tutorials/android/youtubeplayerview-example-in-android-using-youtube-api
