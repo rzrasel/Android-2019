0ed11692fd0aabb313684fbc6135743382399e4b
Rz Rasel <developer@RzRasel.local>
2019-02-25 15:27:47 +0600

    And Aim Game V-201805.00.1 - 2019-02-25 15:27:47

---
+++ AndTestJar/src/com/libs/AndTest.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.libs;
+
+/**
+ *
+ * @author Rz Rasel
+ */
+public class AndTest {
+
+    public static void justLog(String argMessage) {
+        System.out.println("I can print: " + argMessage);
+    }
+}
